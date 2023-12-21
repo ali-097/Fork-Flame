e16450d4c8e192dca917af02298babd44e0330a1
daredevil-15 <sabaikhlaq24@gmail.com>
2023-12-21 12:56:28 +0500

    Added photos in the galleryPage

--- Client/src/components/GalleryPage/Gallery.jsx
+++ Client/src/components/GalleryPage/Gallery.jsx
@@ -1,7 +1,35 @@
 import "./Gallery.css";
+import NavBar from "../Utility/NavBar/NavBar.jsx";
 
 const Gallery = () => {
-	return <div>Gallery</div>;
+	return(
+		<div className='gallery--page'>
+			<NavBar />
+			<div className='gallery--img'>
+				<img
+					src='/gallery/gallery1.png'
+					alt='gallery'
+				/>
+				<img
+					src='/gallery/gallery2.png'
+					alt='gallery'
+				/>
+				<img
+					src='/gallery/gallery3.png'
+					alt='gallery'
+				/>
+				<img
+					src='/gallery/gallery4.png'
+					alt='gallery'
+				/>
+				<img
+					src='/gallery/gallery5.png'
+					alt='gallery'
+				/>
+			</div>
+			
+		</div>
+	);
 };
 
 export default Gallery;
